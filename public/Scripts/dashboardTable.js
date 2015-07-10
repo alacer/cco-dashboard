86f040d55c86836c2ab12778f04ec11a7c3ccf72
krseibel <krseibel@gmail.com>
2015-07-10 12:16:21 -0700

    Date 6/21/15 now appears in date box on dashboard on load

--- public/Scripts/dashboardTable.js
+++ public/Scripts/dashboardTable.js
@@ -5,14 +5,23 @@ $(document).ready(function() {
 /*    var $datepicker = $( "#date_input" );
         $datepicker.datepicker();
         $datepicker.datepicker('setDate', new Date("2015-06-21"));*/
+   /* $("window").on("load", function(){
+        window.location.href = "/dashboard?date=2015-06-21";     
+    }); */
+   
 
-/*    $("window").on("load", function(){
-        $("#date_input").val(new Date("2015-06-21"))      
-    }); */   
+    $('#dp-ex-3').datepicker ({
+        daysOfWeekDisabled: [1,2,3,4,5,6]
+    });
 
+
+   // $('#dp-ex-3').datepicker ("update", '2015-06-21');
+    
     $("#date_input").on("change", function () {
         window.location.href = "/dashboard?date=" +  this.value;
-    });
+    });    
+
+
 
     $("#metricTrends").on("change", function () {
         window.location.href = "/trends?metric=" +  this.value;
@@ -34,9 +43,15 @@ $(document).ready(function() {
     }
   });
 
+    if($("#date_input").val()!="2015-06-21"){
 	var initial_date = window.location.search;
 	var set_date 	 = initial_date.replace("?date=",'');
 		$("#date_input").val(set_date);
+    };
+
+    if($("#date_input").val()===""){
+        $("#date_input").val("2015-06-21")
+    };
     
 	var initial_metric = window.location.search;
 	var set_metric 	 = initial_metric.replace("?metric=",'');
